5e831b859cdfdb91c37051f8a3b0c3286ba3d8b1
ramu <test@test.com>
2026-05-24 08:04:59 +0400

    target files are  deleted

---
+++ customer-servive/src/main/java/com/me/learning/customer/customerservive/CustomerServiveApplication.java
@@ -0,0 +1,13 @@
+package com.me.learning.customer.customerservive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerServiveApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run (CustomerServiveApplication.class, args);
+    }
+
+}
---
+++ customer-servive/src/test/java/com/me/learning/customer/customerservive/CustomerServiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.me.learning.customer.customerservive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiveApplicationTests {
+
+    @Test
+    void contextLoads () {
+    }
+
+}
